1edd7c00f2c1228e92930067103c29aaa4acfeb6
Landon Reasoner <reasoner.landon90@gmail.com>
2020-08-20 17:24:34 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -6,7 +6,9 @@ class GetRequester
   
   URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
   
-  def initialize()
+  def initialize(url)
+    URL 
+  end
   
 end
   
